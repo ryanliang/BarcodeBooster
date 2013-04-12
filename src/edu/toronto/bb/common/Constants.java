7c778f9c85d8a7b89b915f6a2e204ad1cd922b46
Ryan Liang <ryanliang@gmail.com>
2013-04-12 09:45:03 -0400

    add symbol per line constants

--- src/edu/toronto/bb/common/Constants.java
+++ src/edu/toronto/bb/common/Constants.java
@@ -3,7 +3,7 @@ package edu.toronto.bb.common;
 public class Constants {
 	
 	public static final int FULL_SCORE = 40;
-	public static final int ENTRY_PER_LINE = 60; // number of characters on one line in a FASTA file and 
+	public static final int SYMBOL_PER_LINE = 60; // number of characters on one line in a FASTA file and 
 	                                             // number of q-scores on one line in a QUAL file. 
 
 }
