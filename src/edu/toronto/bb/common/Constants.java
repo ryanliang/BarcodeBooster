d4e9f5725b06ca865875640686e2d35e239130cb
Ryan Liang <ryanliang@gmail.com>
2013-04-12 09:07:51 -0400

    test run using biojava lib to read FASTA file.

--- src/edu/toronto/bb/common/Constants.java
+++ src/edu/toronto/bb/common/Constants.java
@@ -2,6 +2,8 @@ package edu.toronto.bb.common;
 
 public class Constants {
 	
-	public static final int FULL_SCORE = 40;	
+	public static final int FULL_SCORE = 40;
+	public static final int ENTRY_PER_LINE = 60; // number of characters on one line in a FASTA file and 
+	                                             // number of q-scores on one line in a QUAL file. 
 
 }
--- test/edu/toronto/bb/unitest/TestCase.java
+++ test/edu/toronto/bb/unitest/TestCase.java
@@ -1,18 +1,42 @@
 package edu.toronto.bb.unitest;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
 import org.junit.Before;
+
 import org.junit.Test;
+
+
+import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.io.FastaReaderHelper;
 
 public class TestCase {
 	
+	File fna;
+	
 	@Before
-	public void setUp() {
-		
+	public void setUp() {		
+		fna = new File("C:\\temp\\java_projects\\BarcodeBooster\\test\\sample\\fna"); // at work
 	}
 	
 	@Test
-	public void shouldPass() {
-		
+	public void shouldReadFastaFile() {
+		try {
+			LinkedHashMap<String, DNASequence> dnaSeqMap = FastaReaderHelper.readFastaDNASequence(fna);
+			Iterator<Entry<String, DNASequence>> it = dnaSeqMap.entrySet().iterator();
+			
+			while (it.hasNext()) {
+				Entry<String, DNASequence> entry = (Entry<String, DNASequence>) it.next();
+				System.out.println("ID: " + entry.getKey());
+				System.out.println("Sequence:");
+				System.out.println(entry.getValue());				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
 	}
 
 }
