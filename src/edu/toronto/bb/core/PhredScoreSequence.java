b4eb39320f6639ce65674e1ae6b85048f6dd5df7
Ryan Liang <ryanliang@gmail.com>
2013-04-14 08:19:38 -0400

    Added validation to parsePhredScoreList

--- src/edu/toronto/bb/core/PhredScoreSequence.java
+++ src/edu/toronto/bb/core/PhredScoreSequence.java
@@ -5,46 +5,46 @@ import java.util.Iterator;
 import java.util.List;
 
 public class PhredScoreSequence implements Iterable<PhredScore> {
-	
-	List<PhredScore> phredScores;
-	
-	public PhredScoreSequence(String sequence) {		
-		this.phredScores = parsePhredScoreList(sequence);
-	}
 
-	public PhredScore scoreAt(int index) {
-		return phredScores.get(index);
-	}	
-	
-	public List<PhredScore> getScores() {
-		return phredScores;
-	}
-	
-	public void setScoreAt(PhredScore score, int index) {
-		phredScores.set(index, score);
-	}
-	
-	public int length() {
-		return phredScores.size();
-	}
-	
-	private ArrayList<PhredScore> parsePhredScoreList(String sequence) {				
-		ArrayList<PhredScore> listOfScores = new ArrayList<PhredScore>();
-		
-		if (sequence != null) {
-			String[] rawScores= sequence.split("\\s+");
-			for (String score : rawScores) {
-				listOfScores.add(new PhredScore(Integer.parseInt(score)));
-			}
-		}
-		
-		return listOfScores;
-	}
-	
-	
-	@Override
-	public Iterator<PhredScore> iterator() {		
-		return phredScores.iterator();
-	}
-	
+    List<PhredScore> phredScores;
+
+    public PhredScoreSequence(String sequence) {		
+        this.phredScores = parsePhredScoreList(sequence);
+    }
+
+    public PhredScore scoreAt(int index) {
+        return phredScores.get(index);
+    }	
+
+    public List<PhredScore> getScores() {
+        return phredScores;
+    }
+
+    public void setScoreAt(PhredScore score, int index) {
+        phredScores.set(index, score);
+    }
+
+    public int length() {
+        return phredScores.size();
+    }
+
+    private ArrayList<PhredScore> parsePhredScoreList(String sequence) {				
+        ArrayList<PhredScore> listOfScores = new ArrayList<PhredScore>();
+
+        if (sequence != null) {
+            String[] rawScores= sequence.split("\\s+");
+            for (String score : rawScores) {
+                listOfScores.add(new PhredScore(Integer.parseInt(score)));
+            }
+        }
+
+        return listOfScores;
+    }
+
+
+    @Override
+    public Iterator<PhredScore> iterator() {		
+        return phredScores.iterator();
+    }
+
 }
