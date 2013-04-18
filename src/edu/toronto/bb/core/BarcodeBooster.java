aba51d04df3df89f6db41713313471f5e6dbc655
Ryan Liang <ryanliang@gmail.com>
2013-04-17 22:12:33 -0400

    Finished setting up IO

--- src/edu/toronto/bb/core/BarcodeBooster.java
+++ src/edu/toronto/bb/core/BarcodeBooster.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.LinkedHashMap;
@@ -43,37 +44,48 @@ public class BarcodeBooster {
     private LinkedHashMap<String,DNASequence> dnaSequences;
     private LinkedHashMap<String,PhredScoreSequence> qualSequences;
     
+    private String barcodes; // contains the CSV formats of the barcode
+    
     public BarcodeBooster(File fnaIn, File fnaOut, File qualIn, File qualOut) throws FileNotFoundException {
         this.fnaIn = fnaIn;
         this.fnaOut = fnaOut;
         this.qualIn = qualIn;
-        this.qualOut = qualOut;
-        
-//        fnaInputStream = new FileInputStream(fnaIn);
+        this.qualOut = qualOut;       
         
         initIO();        
     }
     
-    private void initIO() throws FileNotFoundException {
-        fastaReader = new FastaReader<DNASequence, NucleotideCompound>(fnaInputStream, 
+    public void process() throws IOException {
+        dnaSequences = fastaReader.process();
+        qualSequences = phredReader.process();
+        closeIO();
+    }
+    
+    private void initIO() throws FileNotFoundException {        
+        fnaInputStream = new FileInputStream(fnaIn);
+        fnaOutputStream = new FileOutputStream(fnaOut);
+        qualInputStream = new FileInputStream(qualIn);
+        qualOutputStream = new FileOutputStream(qualOut);
+        
+        fastaReader = new FastaReader<DNASequence, NucleotideCompound>(fnaIn, 
                 new GenericFastaHeaderParser<DNASequence, NucleotideCompound>(), 
                 (SequenceCreatorInterface<NucleotideCompound>) new FileProxyDNASequenceCreator(fnaIn, DNACompoundSet.getDNACompoundSet()));
-        
-        FileOutputStream fnaOS = new FileOutputStream(fnaOut);
-        BufferedOutputStream fnaBO = new BufferedOutputStream(fnaOS);
+                
+        BufferedOutputStream fnaBO = new BufferedOutputStream(fnaOutputStream);
         fastaWriter = new FastaWriter<DNASequence, NucleotideCompound>(fnaBO, 
                 dnaSequences.values(),
                 (FastaHeaderFormatInterface<DNASequence, NucleotideCompound>) new GenericFastaHeaderParser<DNASequence, NucleotideCompound>());
         
-        phredReader = new PhredReader(qualIn);
+        phredReader = new PhredReader(qualInputStream);
         
-//        FileOutputStream fnaOS = new FileOutputStream(fnaOut);
-//        BufferedOutputStream fnaBO = new BufferedOutputStream(fnaOS);
-//        phredWriter = new PhredWriter(os, sequences)
-        
+        BufferedOutputStream qualBO = new BufferedOutputStream(qualOutputStream);
+        phredWriter = new PhredWriter(qualBO, qualSequences.values());        
     }
     
-    public void process() {
-        
+    private void closeIO() throws IOException {
+        fnaInputStream.close();
+        fnaOutputStream.close();
+        qualInputStream.close();
+        qualOutputStream.close();
     }
 }
